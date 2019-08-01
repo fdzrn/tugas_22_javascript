8f4439d94894bffa683eb7e1e3ea33ddf657bcd4
fdzrn <fadzriangibran@gmail.com>
2019-08-01 10:17:48 +0700

    Tugas 22 Javascript
    untuk niomic

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function tugas22() {
+ let string = "Saya ingin belajar Bersama";
+ let array = string.split(" ");
+ array.forEach((i,j) => {
+ 	console.log("Item : " + i + " Index ke " + j)
+ });
+}
+tugas22();
